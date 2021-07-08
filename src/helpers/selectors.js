8ca7c699be15e9c790cc9cee392523dd17dd4806
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-07-07 22:25:51 -0400

    added selector function in helper file

---
+++ src/helpers/selectors.js
@@ -0,0 +1,24 @@
+export function getAppointmentsForDay(state, day) {
+  let resultObj = [];
+  for (const key in state.days) {
+    if (Object.hasOwnProperty.call(state.days, key)) {
+      const element = state.days[key];
+      if (element.name === day) {
+        const arr = [...element.appointments];
+
+        for (const appointment in state.appointments) {
+          if (Object.hasOwnProperty.call(state.appointments, appointment)) {
+            const element = state.appointments[appointment];
+            arr.forEach((ele) => {
+              if (element.id === ele) {
+                resultObj.push(element);
+              }
+            });
+          }
+        }
+      }
+    }
+  }
+  return resultObj;
+  //... returns an array of appointments for that day
+}
