b1235475c98f7000e8def0fc53381cf4967a9041
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-07-19 20:06:10 -0400

    code update for making state persistent

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,21 +6,25 @@ export default function useVisualMode(initial) {
 
   //Transition of modes to next or previous
   function transition(nextMode, replace = false) {
-    if (replace) history.pop(); //If we are caught in error scenarios to revert to previous state and maintain proper state we need to pop the last stale state
-
     setMode(nextMode);
-    setHistory((prev) => [...prev, nextMode]); //spread prev state for maintianing immutablity
+    if (replace) {
+      //If we are caught in error scenarios to revert to previous state and maintain proper state we need to pop the last stale state
+      setHistory((prev) => [...prev.slice(0, prev.length - 1), nextMode]);
+    } else {
+      setHistory((prev) => [...prev, nextMode]); //spread prev state for maintianing immutablity
+    }
   }
 
   //This function helps in tracing back the state mode depending upon the stack size
   function back() {
-    if (history.length > 1) {
-      history.pop();
-      setHistory(history);
-      setMode(history[history.length - 1]);
+    let currentHistory = [...history];
+    if (currentHistory.length > 1) {
+      currentHistory.pop();
+      setHistory(currentHistory);
+      setMode(currentHistory[currentHistory.length - 1]);
     } else {
-      setHistory(history);
-      setMode(history[history.length - 1]);
+      setHistory(currentHistory);
+      setMode(currentHistory[currentHistory.length - 1]);
     }
   }
 
