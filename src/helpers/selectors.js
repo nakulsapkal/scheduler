05f1e19cc1e670251e648b60f2f77389da08403b
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-07-23 20:02:12 -0400

    Refactoring of helper functions

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,45 +1,25 @@
 //Helper Function For Getting Appointments Of The Day
 export function getAppointmentsForDay(state, current_day) {
-  let resultAppointmentObj = [];
-  //Looping through state.days to find the current day's appointments
-  for (const key in state.days) {
-    if (Object.hasOwnProperty.call(state.days, key)) {
-      const day = state.days[key];
-      if (day.name === current_day) {
-        const currentDayAppointments = [...day.appointments];
+  //Looping through state.days to find the current day's Obj
+  const currentDayObj = state.days.find(
+    (dayObj) => dayObj.name === current_day
+  );
 
-        //Looping through state.appointments to fetch all details for multiple appointments for the current day
-        for (const key in state.appointments) {
-          if (Object.hasOwnProperty.call(state.appointments, key)) {
-            const appointment = state.appointments[key];
-            currentDayAppointments.forEach((currentDayAppointmentId) => {
-              if (appointment.id === currentDayAppointmentId) {
-                resultAppointmentObj.push(appointment);
-              }
-            });
-          }
-        }
-      }
-    }
-  }
-  return resultAppointmentObj;
+  if (state.days.length === 0 || currentDayObj === undefined) return [];
+
+  //mapping through currentDayObj.appointments to fetch all details for multiple appointments for the current day
+  return currentDayObj.appointments.map((id) => state.appointments[id]);
 }
 
 //Fetching An Interview For Every Appointment If Any Exists
-export function getInterview(state, interview) {
-  let resultInterviewObj = {};
-
-  if (interview) {
-    for (const interviewer in state.interviewers) {
-      if (Object.hasOwnProperty.call(state.interviewers, interviewer)) {
-        const interviewingPerson = state.interviewers[interviewer];
-        if (interviewingPerson.id === interview.interviewer) {
-          resultInterviewObj.student = interview.student;
-          resultInterviewObj.interviewer = interviewingPerson;
-        }
-      }
-    }
-    return resultInterviewObj;
+export function getInterview(state, interviewDetails) {
+  if (interviewDetails) {
+    return {
+      ...interviewDetails,
+      interviewer: state.interviewers.find(
+        (interviewer) => interviewDetails.interviewer === interviewer.id
+      ),
+    };
   } else {
     return null;
   }
@@ -47,27 +27,12 @@ export function getInterview(state, interview) {
 
 //Fetching An Interviews For Every Day In Schedule
 export function getInterviewersForDay(state, current_day) {
-  let resultInterviewersObj = [];
   //Looping through state.days to find the current day's interviewers
-  for (const key in state.days) {
-    if (Object.hasOwnProperty.call(state.days, key)) {
-      const day = state.days[key];
-      if (day.name === current_day) {
-        const currentDayInterviewers = [...day.interviewers];
+  const currentDayObj = state.days.find(
+    (dayObj) => current_day === dayObj.name
+  );
 
-        //Looping through state.appointments to fetch all details for multiple appointments for the current day
-        for (const key in state.interviewers) {
-          if (Object.hasOwnProperty.call(state.interviewers, key)) {
-            const interview = state.interviewers[key];
-            currentDayInterviewers.forEach((currentDayInterviewerId) => {
-              if (interview.id === currentDayInterviewerId) {
-                resultInterviewersObj.push(interview);
-              }
-            });
-          }
-        }
-      }
-    }
-  }
-  return resultInterviewersObj;
+  if (state.days.length === 0 || currentDayObj === undefined) return [];
+  // mapping through currentDayObj.interviewers to fetch all details for multiple interviewers for the current day
+  return currentDayObj.interviewers.map((id) => state.interviewers[id - 1]);
 }
